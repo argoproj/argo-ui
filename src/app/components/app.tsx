f500883a36db1caf81ecf33957eb5347e70a3259
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-01-22 21:26:32 -0800

    Initial commit

---
+++ src/app/components/app.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const App = () => <h1>Hello world! </h1>;
---
+++ src/app/webpack.config.js
@@ -0,0 +1,38 @@
+'use strict;';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+    entry: './src/app/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/../../dist/app'
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader?configFileName=./src/app/tsconfig.json' },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/app/index.html'
+        })
+    ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
+module.exports = config;
