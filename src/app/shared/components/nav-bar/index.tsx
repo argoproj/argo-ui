4fb69683042af3cb717211729abd1b21b8843eba
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-01-30 17:08:14 -0800

    Remove unnecessary connect usage in NavBar component

--- src/app/shared/components/nav-bar/index.tsx
+++ src/app/shared/components/nav-bar/index.tsx
@@ -1,24 +1,23 @@
 import * as classNames from 'classnames';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import { connect } from 'react-redux';
 
-import { AppContext, AppState } from '../../redux';
+import { AppContext } from '../../redux';
 
 require('./nav-bar.scss');
 
 export interface NavBarProps extends React.Props<any> {
     items: Array<{ path: string; iconClassName: string; title: string; }>;
-    routerPath?: string;
 }
 
-const component: React.StatelessComponent = (props: NavBarProps, context: { router: AppContext }) => {
+export const NavBar: React.StatelessComponent<NavBarProps> = (props: NavBarProps, context: { router: AppContext }) => {
+    const routerPath = context.router.route.location.pathname;
     return (
         <div className='nav-bar'>
         <div className='nav-bar__logo'>
             <img src='/assets/images/logo.png' alt='Argo'/>
             {(props.items || []).map((item) => (
-                <div className={classNames('nav-bar__item', { active: props.routerPath === item.path })}
+                <div className={classNames('nav-bar__item', { active: routerPath === item.path })}
                      key={item.path + item.title}
                      onClick={() => context.router.history.push(item.path)}>
 
@@ -31,12 +30,7 @@ const component: React.StatelessComponent = (props: NavBarProps, context: { rout
     </div>);
 };
 
-component.contextTypes = {
+NavBar.contextTypes = {
     router: PropTypes.object,
     store: PropTypes.object,
 };
-
-export const NavBar = connect<{}, {}, NavBarProps, AppState<any>>((state) => ({
-    routerPath: state.router.location.pathname,
-    router: state.router,
-}))(component);
