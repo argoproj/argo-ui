08903d33709f348b7fe192f73408535d54163180
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-02-06 13:05:27 -0800

    Page top bar should always stay at the top

--- src/app/shared/components/page/page.tsx
+++ src/app/shared/components/page/page.tsx
@@ -1,3 +1,4 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
 import { TopBar, TopBarProps } from '../top-bar/top-bar';
 
@@ -6,8 +7,10 @@ require('./page.scss');
 type PageProps = TopBarProps;
 
 export const Page = (props: PageProps) => (
-    <div className='page'>
-        <TopBar {...props}/>
+    <div className={classNames('page', { 'page--has-toolbar': !!props.filter })}>
+        <div className='page__top-bar'>
+            <TopBar {...props}/>
+        </div>
         <div className='page__content-wrapper'>
             {props.children}
         </div>
