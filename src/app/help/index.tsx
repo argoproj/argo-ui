b38afa68ab9fa990b5686ebd56b18f644b2f4b32
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-05-29 09:26:23 -0700

    Issue #858 - Blank help page in Argo UI when using browser navigation

--- src/app/help/index.tsx
+++ src/app/help/index.tsx
@@ -3,6 +3,6 @@ import { Help } from './components/help';
 export default {
     component: Help,
     reducer: (state: any, action: any) => {
-        // do nothing
+        return state || {};
     },
 };
