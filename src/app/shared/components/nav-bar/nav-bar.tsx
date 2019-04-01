f378862aa7b220ed5fb721ee9097b6147e3e9b9c
Alex Collins <alex_collins@intuit.com>
2019-04-01 13:53:05 -0700

    lint

--- src/app/shared/components/nav-bar/nav-bar.tsx
+++ src/app/shared/components/nav-bar/nav-bar.tsx
@@ -1,7 +1,7 @@
+import Tippy from '@tippy.js/react';
 import * as classNames from 'classnames';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import Tippy from '@tippy.js/react'
 
 import { AppContext } from '../../context';
 
@@ -22,7 +22,7 @@ export const NavBar: React.StatelessComponent<NavBarProps> = (props: NavBarProps
         <div className='nav-bar__logo'>
             <img src='assets/images/logo.png' alt='Argo'/>
             {(props.items || []).map((item) => (
-                <Tippy content={item.title} placement="right" arrow={true}>
+                <Tippy content={item.title} placement='right' arrow={true}>
                     <div className={classNames('nav-bar__item', { active: isActiveRoute(locationPath, item.path) })}
                         key={item.path + item.title}
                         onClick={() => context.router.history.push(item.path)}>
