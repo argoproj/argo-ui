07448f624bcd909ea8f6e9bb25af23665e07d80e
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-04-22 11:48:57 -0700

    Change tab badge type to string

---
+++ stories/tabs.tsx
@@ -0,0 +1,17 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { Tabs } from '../src/app/shared/components';
+
+storiesOf('Tabs', module)
+    .add('basic tabs', () => (
+        <Tabs tabs={[{
+            title: 'Tab 1',
+            content: <p>Tab 1 content</p>,
+            key: 'tab1',
+        }, {
+            title: 'Tab 2 with badge',
+            content: <p>Tab 2 content</p>,
+            key: 'tab2',
+            badge: '5',
+        }]}/>
+    ));
