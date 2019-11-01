89eccacb590cb207d575eb87aece9f84ecc6d6ca
Alex Collins <alexec@users.noreply.github.com>
2019-11-01 12:37:18 -0700

    Adds Circle CI set-up (#44)

--- src/app/shared/components/help-icon/help-icon.tsx
+++ src/app/shared/components/help-icon/help-icon.tsx
@@ -1,5 +1,5 @@
-import {Tooltip} from '../tooltip/tooltip';
 import * as React from 'react';
+import {Tooltip} from '../tooltip/tooltip';
 
 export const HelpIcon = ({title}: {title: React.ReactChild | React.ReactChild[]}) => (
     <Tooltip content={title}>
