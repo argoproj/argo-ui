cf1a545ebe6cc6c315aea0b0e4f2fff6801e7074
Daniel Herman <dherman@factset.com>
2020-10-04 17:58:26 -0400

    fix(page): correct typing errors
    Signed-off-by: Daniel Herman <dherman@factset.com>

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -20,36 +20,40 @@ export interface PageContextProps {
 
 export const PageContext = React.createContext<PageContextProps>({ title: 'Argo' });
 
-export const Page = (props: PageProps) => (
-    <div className={classNames('page', { 'page--has-toolbar': !!props.toolbar })}>
-        <React.Fragment>
-            <DataLoader input={new Date()} load={() => Utils.toObservable(props.toolbar)}>
-            {(toolbar: Toolbar) => (
-                <React.Fragment>
-                    <PageContext.Consumer>
-                        {(ctx) => {
-                            let titleParts = [ctx.title];
-                            if (toolbar && toolbar.breadcrumbs && toolbar.breadcrumbs.length > 0) {
-                                titleParts = [toolbar.breadcrumbs.map((item) => item.title).reverse().join(' / ')].concat(titleParts);
-                            } else if (props.title) {
-                                titleParts = [props.title].concat(titleParts);
-                            }
-                            return (
-                                <Helmet>
-                                    <title>{titleParts.join(' - ')}</title>
-                                </Helmet>
-                            );
-                        }}
-                    </PageContext.Consumer>
-                    <div className='page__top-bar'>
-                        <TopBar title={props.title} toolbar={toolbar}/>
-                    </div>
-                </React.Fragment>
-            )}
-            </DataLoader>
-            <div className='page__content-wrapper'>
-                {props.children}
-            </div>
-        </React.Fragment>
-    </div>
-);
+export const Page = (props: PageProps) => {
+    const toolbarObservable = props.toolbar && Utils.toObservable(props.toolbar);
+
+    return (
+        <div className={classNames('page', { 'page--has-toolbar': !!props.toolbar })}>
+            <React.Fragment>
+                {toolbarObservable && <DataLoader input={new Date()} load={() => toolbarObservable}>
+                {(toolbar: Toolbar) => (
+                    <React.Fragment>
+                        <PageContext.Consumer>
+                            {(ctx) => {
+                                let titleParts = [ctx.title];
+                                if (toolbar && toolbar.breadcrumbs && toolbar.breadcrumbs.length > 0) {
+                                    titleParts = [toolbar.breadcrumbs.map((item) => item.title).reverse().join(' / ')].concat(titleParts);
+                                } else if (props.title) {
+                                    titleParts = [props.title].concat(titleParts);
+                                }
+                                return (
+                                    <Helmet>
+                                        <title>{titleParts.join(' - ')}</title>
+                                    </Helmet>
+                                );
+                            }}
+                        </PageContext.Consumer>
+                        <div className='page__top-bar'>
+                            <TopBar title={props.title} toolbar={toolbar}/>
+                        </div>
+                    </React.Fragment>
+                )}
+                </DataLoader>}
+                <div className='page__content-wrapper'>
+                    {props.children}
+                </div>
+            </React.Fragment>
+        </div>
+    );
+};
