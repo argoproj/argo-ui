ff9250ce87b6deb3ddfc70641097b832c208ac91
Daniel Herman <dherman@factset.com>
2020-12-14 22:12:46 -0500

    test(popup-manager): add partial unit tests for popup manager
    Signed-off-by: Daniel Herman <dherman@factset.com>

---
+++ src/components/popup/popup-manager.spec.tsx
@@ -0,0 +1,50 @@
+import { mount } from 'enzyme';
+import {Observable, TestScheduler} from 'rxjs';
+import { Popup, PopupProps } from './popup';
+import {PopupManager} from './popup-manager';
+
+describe('PopupManager', () => {
+    let scheduler: TestScheduler;
+
+    beforeEach(() => {
+        scheduler = new TestScheduler((a, b) => expect(a).toEqual(b));
+    });
+
+    describe('confirm', () => {
+        it.each([
+            ['OK', true, '[qe-id="argo-popup-ok-button"]'],
+            ['Cancel', false, '[qe-id="argo-popup-cancel-button"]'],
+        ])('%s', async (_, promiseResult, btnSelector) => {
+            const fn = jest.fn<void, [null | PopupProps]>();
+            const manager = new PopupManager();
+
+            Observable.from(manager.popupProps, scheduler).subscribe(fn);
+            scheduler.flush();
+
+            expect(fn).toHaveBeenCalledTimes(1);
+            expect(fn).toHaveBeenLastCalledWith(null);
+
+            const promise = manager.confirm('foo', 'bar');
+            scheduler.flush();
+
+            expect(fn).toHaveBeenCalledTimes(2);
+
+            const props = fn.mock.calls[1][0]!;
+
+            expect(typeof props).toBe('object');
+
+            const popup = mount(Popup(props));
+
+            expect(popup.find('.popup-container__header').text().trim()).toBe('foo');
+            expect(popup.find('.popup-container__body').text()).toBe('bar');
+
+            popup.find(btnSelector).simulate('click');
+
+            await expect(promise).resolves.toBe(promiseResult);
+            scheduler.flush();
+
+            expect(fn).toHaveBeenCalledTimes(3);
+            expect(fn).toHaveBeenLastCalledWith(null);
+        });
+    });
+});
