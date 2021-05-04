f3a82a0f42a8210734e2a22caf26f792e4666e08
Remington Breeze <remington@breeze.software>
2021-05-04 11:49:00 -0700

    Remove old v2 dir
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- src/v2/README.md
+++
@@ -1,3 +0,0 @@
-# Argo UI v2 Components
-
-Version 2 components are all based on React Hooks, and contain inline documentation.
--- src/v2/components/text/text.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import './text.scss';
-
-export const Text = (props: {children: string | string[]}) => {
-    return <div className='text'>{props.children}</div>;
-};
-
-export default Text;
