0aeaf4349ef9749bda350c00115c93e896445bc7
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2021-10-19 16:34:53 +0300

    updated design of text buttons
    Signed-off-by: viktorplakida <plakyda1@gmail.com>

--- v3/components/button/button.stories.tsx
+++ v3/components/button/button.stories.tsx
@@ -1,5 +1,7 @@
+import { DownloadOutlined } from '@ant-design/icons';
 import {Meta} from '@storybook/react';
 import * as React from 'react';
+
 import { Button } from '../../index';
 
 import '../../antd.less';
@@ -29,6 +31,7 @@ const argg: Meta = {
         children: {control: {type: 'text'}},
         disabled: {control: {type: 'boolean'}},
         danger: {control: {type: 'boolean'}},
+        icon: {control: {type: 'boolean'}},
     },
     args: {
         type: 'default',
@@ -37,80 +40,56 @@ const argg: Meta = {
         children: 'My button',
         disabled: false,
         danger: false,
+        icon: false,
     },
 };
 
 export default argg;
 
-export const Primary = (args: any) => {
+const ButtonStoryComponent = (args: any) => {
     return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
+        <div style={{width: '50%', marginBottom: '1em'}}>
+            <Button {...args} icon={args.icon && <DownloadOutlined />} />
+        </div>
     );
 };
+
+export const Primary = (args: any) => <ButtonStoryComponent {...args} />;
 
 Primary.args = {
     type: 'primary',
     children: 'My button',
 };
 
-export const Default = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
-};
+export const Default = (args: any) => <ButtonStoryComponent {...args} />;
 
 Default.args = {
     type: 'secondary',
     children: 'My button',
 };
 
-export const Ghost = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
-};
+export const Ghost = (args: any) => <ButtonStoryComponent {...args} />;
 
 Ghost.args = {
     type: 'ghost',
     children: 'My button',
 };
 
-export const Text = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
-};
+export const Text = (args: any) => <ButtonStoryComponent {...args} />;
 
 Text.args = {
     type: 'text',
     children: 'My button',
 };
 
-export const Danger = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
+export const Disabled = (args: any) => <ButtonStoryComponent {...args} />;
+
+Disabled.args = {
+    children: 'My button',
+    disabled: true,
 };
+
+export const Danger = (args: any) => <ButtonStoryComponent {...args} />;
 
 Danger.args = {
     type: 'primary',
@@ -118,15 +97,7 @@ Danger.args = {
     children: 'My button',
 };
 
-export const DangerGhost = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
-};
+export const DangerGhost = (args: any) => <ButtonStoryComponent {...args} />;
 
 DangerGhost.args = {
     type: 'primary',
@@ -135,15 +106,7 @@ DangerGhost.args = {
     children: 'My button',
 };
 
-export const DangerText = (args: any) => {
-    return (
-        <React.Fragment>
-            <div style={{width: '50%', marginBottom: '1em'}}>
-                <Button {...args} />
-            </div>
-        </React.Fragment>
-    );
-};
+export const DangerText = (args: any) => <ButtonStoryComponent {...args} />;
 
 DangerText.args = {
     type: 'text',
