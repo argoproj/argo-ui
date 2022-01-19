b7a057c465b2659ae3a0d2f9574caa59f15c2ae6
Ken Kaizu <k.kaizu38@gmail.com>
2022-01-20 00:33:12 +0900

    feat: customize nav bar background color style (#167)
    Signed-off-by: krrrr38 <k.kaizu38@gmail.com>

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
-import { NavBar } from '../nav-bar/nav-bar';
+import {NavBar, NavBarStyle} from '../nav-bar/nav-bar';
 
 require('./layout.scss');
 
 export interface LayoutProps {
     navItems: Array<{ path: string; iconClassName: string; title: string; }>;
     version?: () => React.ReactElement;
+    navBarStyle?: NavBarStyle;
     children?: React.ReactNode;
 }
 
 export const Layout = (props: LayoutProps) => (
     <div className='layout'>
-        <NavBar items={props.navItems} version={props.version}/>
+        <NavBar items={props.navItems} version={props.version} style={props.navBarStyle} />
         {props.children}
     </div>
 );
