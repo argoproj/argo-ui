4303ff84372c3110592f0a60faa49ed7379dcc5a
Keith Chong <kykchong@redhat.com>
2022-03-28 20:39:57 -0400

    feat: Escape key should close sliding window in UI (#8225)
    Signed-off-by: Keith Chong <kykchong@redhat.com>

--- src/components/sliding-panel/sliding-panel.tsx
+++ src/components/sliding-panel/sliding-panel.tsx
@@ -1,5 +1,6 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
+import { Key, KeybindingContext, KeybindingProvider } from '../../../v2';
 
 export interface SlidingPanelProps extends React.Props<any> {
     isShown?: boolean;
@@ -15,36 +16,76 @@ export interface SlidingPanelProps extends React.Props<any> {
 
 require('./sliding-panel.scss');
 
-export const SlidingPanel = (props: SlidingPanelProps) => (
-    <div className={classNames('sliding-panel', {
-        'sliding-panel--has-header': !!props.header,
-        'sliding-panel--has-footer': !!props.footer,
-        'sliding-panel--is-narrow': props.isNarrow,
-        'sliding-panel--is-middle': props.isMiddle,
-        'sliding-panel--opened': props.isShown,
-        'sliding-panel--no-padding': props.hasNoPadding,
-        'sliding-panel--off-canvas': props.offCanvas,
-    })}>
-        <div className='sliding-panel__wrapper'>
-            <button className='sliding-panel__close' aria-hidden='true' onClick={() => props.onClose && props.onClose()}>
-                <span>
-                    <i className='argo-icon-close' aria-hidden='true'/>
-                </span>
-            </button>
-            {props.header && (
-                <div className={classNames('sliding-panel__header', {'sliding-panel__header--close-btn-right-padding': props.hasCloseButton})}>
-                    {props.header}
+export const SlidingPanel = (props: SlidingPanelProps) => {
+    return (
+        <KeybindingProvider>
+            <RenderSlidingPanel {...props} />
+        </KeybindingProvider>
+    );
+};
+
+const RenderSlidingPanel = (props: SlidingPanelProps) => {
+
+    const {useKeybinding} = React.useContext(KeybindingContext);
+
+    const closeButtonRef = React.useRef(null);
+    const bodyDivRef = React.useRef(null);
+    const panelHeaderDivRef = React.useRef(null);
+    const panelFooterDivRef = React.useRef(null);
+
+    React.useEffect(() => {
+        if (closeButtonRef && closeButtonRef.current) {
+            closeButtonRef.current.focus();
+        }
+    }, []);
+
+    useKeybinding({
+        keys: Key.ESCAPE,
+        action: () => {
+            if (props.isShown && props.onClose) {
+                props.onClose();
+                return true;
+            }
+            return false;
+        },
+        combo: false,
+        target: [closeButtonRef, bodyDivRef, panelHeaderDivRef, panelFooterDivRef],
+    });
+
+    return (
+        <div className={classNames('sliding-panel', {
+            'sliding-panel--has-header': !!props.header,
+            'sliding-panel--has-footer': !!props.footer,
+            'sliding-panel--is-narrow': props.isNarrow,
+            'sliding-panel--is-middle': props.isMiddle,
+            'sliding-panel--opened': props.isShown,
+            'sliding-panel--no-padding': props.hasNoPadding,
+            'sliding-panel--off-canvas': props.offCanvas,
+        })}>
+            <div className='sliding-panel__wrapper'>
+                {props.isShown && (
+                    <button autoFocus={true} className='sliding-panel__close' aria-hidden='true' onClick={() => props.onClose && props.onClose()} ref={closeButtonRef}>
+                        <span>
+                            <i className='argo-icon-close' aria-hidden='true'/>
+                        </span>
+                    </button>
+                )}
+                {props.header && (
+                    <div className={classNames('sliding-panel__header', {'sliding-panel__header--close-btn-right-padding': props.hasCloseButton})}
+                        ref={panelHeaderDivRef} tabIndex={-1}>
+                        {props.header}
+                    </div>
+                )}
+                <div className='sliding-panel__body' ref={bodyDivRef} tabIndex={-1}>
+                    {props.children}
                 </div>
-            )}
-            <div className='sliding-panel__body'>
-                {props.children}
+                {props.footer && (
+                    <div className='sliding-panel__footer' ref={panelFooterDivRef} tabIndex={-1}>
+                        {props.footer}
+                    </div>
+                )}
             </div>
-            {props.footer && (
-                <div className='sliding-panel__footer'>
-                    {props.footer}
-                </div>
-            )}
+            <div className='sliding-panel__outside' onClick={() => props.onClose && props.onClose()}/>
         </div>
-        <div className='sliding-panel__outside' onClick={() => props.onClose && props.onClose()}/>
-    </div>
-);
+    );
+};
