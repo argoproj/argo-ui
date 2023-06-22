9eb1b9e4d33d30e9041c4f7c6a26af47869705e9
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2023-06-22 14:04:31 -0700

    restore animation property
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -4,5 +4,5 @@ import 'tippy.js/dist/tippy.css';
 import 'tippy.js/themes/light.css';
 
 export const Tooltip = ( props: TippyProps ) => (
-    <Tippy appendTo={document.body}  theme='light' interactive={true} {...props} />
+    <Tippy animation='fade' appendTo={document.body}  theme='light' interactive={true} {...props} />
 );
