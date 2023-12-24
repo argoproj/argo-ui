6d1afb356fa8ada4cb4dff9f6aa7deaaeb439626
Avhi Mazumder <avhimazumder5@outlook.com>
2023-12-24 10:57:09 +0530

    fixed to #16375
    Signed-off-by: Avhi Mazumder <avhimazumder5@outlook.com>

--- src/components/notifications/notifications.tsx
+++ src/components/notifications/notifications.tsx
@@ -28,14 +28,30 @@ export class Notifications extends React.Component<NotificationsProps> {
         super(props);
         this.subscription = props.notifications.subscribe((next) => {
             let toastMethod = toast.success;
+            let toastOptions: any = {
+                position: toast.POSITION.BOTTOM_RIGHT,
+                closeOnClick: false,
+                pauseOnHover: true,
+                pauseOnFocusLoss: true,
+                draggable: false,
+                autoClose: AUTO_CLOSE_TIMEOUT,
+                style: {} // Initialize style object
+            };
+
             switch (next.type) {
                 case NotificationType.Error:
                     toastMethod = toast.error;
+                    toastOptions.style = {
+                        color: '#fff', // Change text color to white
+                        backgroundColor: '#d32f2f', // Change background color to a high-contrast red
+                        /* Add any other styles to enhance contrast */
+                    };
                     break;
                 case NotificationType.Warning:
                     toastMethod = toast.warn;
                     break;
             }
+
             toastMethod((
                 <div onClick={(e) => {
                     const sel = window.getSelection();
@@ -50,14 +66,7 @@ export class Notifications extends React.Component<NotificationsProps> {
                 }}>
                     {next.content}
                 </div>
-            ), {
-                position: toast.POSITION.BOTTOM_RIGHT,
-                closeOnClick: false,
-                pauseOnHover: true,
-                pauseOnFocusLoss: true,
-                draggable: false,
-                autoClose: AUTO_CLOSE_TIMEOUT,
-            });
+            ), toastOptions);
         });
     }
 
@@ -69,6 +78,19 @@ export class Notifications extends React.Component<NotificationsProps> {
     }
 
     public render() {
-        return  <ToastContainer />;
+        const toastStyles = 
+            .Toastify__toast--error {
+                color: #fff; /* Change text color to white */
+                background-color: #d32f2f; /* Change background color to a high-contrast red */
+                /* Add any other styles to enhance contrast */
+            }
+        ;
+
+        return (
+            <>
+                <ToastContainer />
+                <style>{toastStyles}</style>
+            </>
+        );
     }
 }
