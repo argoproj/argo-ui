8cdc59a3f41b207e784125091eea8903d6d84d3e
Anton Gilgur <agilgur5@gmail.com>
2024-06-05 19:54:42 -0400

    fix incorrect parameter naming
    - apparently `durationMs` was incorrectly named as it actually took seconds, not milliseconds
    - for backward compat, leave `durationMs`, but also introduce `durationS`
      - and add JSDoc to indicate this to users of the component
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- src/components/duration.tsx
+++ src/components/duration.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 
 import { formatDuration } from '../../v2';
 
-export function Duration(props: {durationMs: number}) {
-    return <span>{formatDuration(props.durationMs / 1000)}</span>;
+/**
+ * Output a string duration from a number of seconds
+ *
+ * @param {number} props.durationS - The number of seconds.
+ * @param {number} props.durationMs - The number of seconds. DEPRECATED: The "Ms" suffix is incorrect, use props.durationS instead.
+ */
+export function Duration(props: {durationMs: number, durationS: number}) {
+    return <span>{formatDuration(props.durationMs || props.durationS)}</span>;
 }
