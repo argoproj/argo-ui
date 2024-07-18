d7a4c73c54f2b229b7952eb8114cb853eeefafb1
Anton Gilgur <agilgur5@gmail.com>
2024-07-18 15:28:12 -0400

    migrate from `@dump247/storybook-state` to `useState` hook
    - as per its archived docs
    - tried to make the diff as small as possible
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- stories/select.tsx
+++ stories/select.tsx
@@ -1,35 +1,36 @@
-import { Store, withState } from '@dump247/storybook-state';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import { Select } from '../src/components';
 
 storiesOf('Select', module)
-    .add('default', withState({ selected: 'option1' })(({store}: { store: Store<any> }) => (
-        () => (
+    .add('default', () => {
+        const [selected, setSelected] = React.useState('option1');
+        return (
             <div>
                 <h4>
-                    Selected option value: {store.state.selected}
-                    <button className='argo-button argo-button--base' onClick={() => store.set({ selected: 'option2' })} >Select option 2</button>
+                    Selected option value: {selected}
+                    <button className='argo-button argo-button--base' onClick={() => setSelected('option2')} >Select option 2</button>
                 </h4>
                 <Select
-                    value={store.state.selected}
+                    value={selected}
                     placeholder='Select something'
                     options={['option1', { value: 'option2', title: 'Option 2' }]}
-                    onChange={(option) => store.set({ selected: option.value })}
+                    onChange={(option) => setSelected(option.value)}
                     />
             </div>
-        ))),
-    ).add('multi-select', withState({ selected: 'option1' })(({store}: { store: Store<any> }) => (
-        () => (
+        )},
+    ).add('multi-select', () => {
+        const [selected, setSelected] = React.useState(['option1']);
+        return (
             <div>
                 <Select
-                    value={store.state.selected}
+                    value={selected}
                     multiSelect={true}
                     placeholder='Select something'
                     options={['option1', { value: 'option2', title: 'Option 2' }]}
-                    onMultiChange={(options) => store.set({ selected: options.map((item) => item.value) })}
+                    onMultiChange={(options) => setSelected(options.map((item) => item.value))}
                     />
             </div>
-        ))),
+        )},
     );
