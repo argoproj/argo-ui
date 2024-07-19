2f491b4e0916f39e64500a94b4cc924f6a8c614d
Anton Gilgur <agilgur5@gmail.com>
2024-07-19 12:52:29 -0400

    remove not working CSS test
    - it was tested with and without the comment, to be specific
      - as well as several other ways
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- stories/data-loader.tsx
+++ stories/data-loader.tsx
@@ -5,8 +5,6 @@ import { App } from './utils';
 
 import { DataLoader } from '../src/components/data-loader';
 
-// import '../src/styles/main.scss';
-
 function loadData(input: string): Promise<string> {
     return new Promise((resolve) => window.setTimeout(() => resolve(`hello ${input}`), 50));
 }
