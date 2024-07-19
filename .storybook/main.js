8db46bd6d4880f847927f5ed75cb5c09f6e2aa1a
Anton Gilgur <agilgur5@gmail.com>
2024-07-19 12:09:36 -0400

    improve perf with built in loader for TS
    - remove `ts-loader` as not necessary
    - plus some settings for Storybook's TS loader preset
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,13 @@ const path = require('path');
 module.exports = {
     stories: ['../stories/*.stories.tsx'],
     addons: ['@storybook/addon-essentials'],
+    typescript: {
+        check: false, // typecheck separately
+        reactDocgen: false, // substantially improves performance: https://github.com/storybookjs/storybook/issues/22164#issuecomment-1603627308
+    },
     webpackFinal: async (config, {configType}) => {
+        config.devtool = false; // perf per: https://github.com/storybookjs/storybook/issues/19736#issuecomment-1478103817
         config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            loader: 'ts-loader'
-        }, {
             test: /\.scss$/,
             exclude: /node_modules/,
             include: path.resolve(__dirname, '../'),
