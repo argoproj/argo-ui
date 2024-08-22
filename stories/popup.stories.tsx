80f3ce95d35c2f7d06ffd81b836406eb49089241
Anton Gilgur <agilgur5@gmail.com>
2024-08-22 17:54:09 -0400

    missing trailing comma
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- stories/popup.stories.tsx
+++ stories/popup.stories.tsx
@@ -305,7 +305,7 @@ export const PromptWithReactCheckboxThatIsCheckedByDefaultUsernameDefaultSetToAd
                         },
                         undefined,
                         undefined,
-                        {checkboxField: true, username: 'admin'}
+                        {checkboxField: true, username: 'admin'},
                     );
                     action('Prompt values')(values);
                 }}>Click me</button>
