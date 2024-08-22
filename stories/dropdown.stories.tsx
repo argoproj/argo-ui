e6e2da1f78d8e0c0a6addb6d679cbd4974f5d98f
Anton Gilgur <agilgur5@gmail.com>
2024-08-22 17:22:15 -0400

    diff reduction
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- stories/dropdown.stories.tsx
+++ stories/dropdown.stories.tsx
@@ -10,20 +10,24 @@ export default {
 export const Default = () => (<DropDown anchor={() => <a>Click me</a>}><p>Dropdown content here</p></DropDown>);
 Default.storyName = 'default';
 
-export const Menu = () => (
-    <DropDown isMenu={true} anchor={() => <a>Click me</a>}>
-        <ul>
-            <li><a>menu item 1</a></li>
-            <li><a>menu item 2</a></li>
-        </ul>
-    </DropDown>
-);
+export const Menu = () => {
+    return (
+        <DropDown isMenu={true} anchor={() => <a>Click me</a>}>
+            <ul>
+                <li><a>menu item 1</a></li>
+                <li><a>menu item 2</a></li>
+            </ul>
+        </DropDown>
+    )
+};
 Menu.storyName = 'menu';
 
-export const MenuWrapper = () => (
-    <DropDownMenu anchor={() => <a>Click me</a>} items={[{
-        title: 'menu item 1',
-        action: () => window.alert('Clicked!'),
-    }]} />
-);
+export const MenuWrapper = () => {
+    return (
+        <DropDownMenu anchor={() => <a>Click me</a>} items={[{
+            title: 'menu item 1',
+            action: () => window.alert('Clicked!'),
+        }]} />
+    )
+};
 MenuWrapper.storyName = 'menu wrapper';
