638b80bbf48114d746f3304cdb4430a54936db7b
Arman Ossi Loko <armanossiloko@outlook.com>
2025-03-27 15:00:06 +0100

    fix: prevent double slashes in path handling - NavigationManager.goto method
    - Adjusted the `NavigationManager.goto` method to handle redirection to `./` paths when the current URL ends with a slash - to avoid ending up with duplicate slashes in the URL
    
    Signed-off-by: Arman Ossi Loko armanossiloko@gmail.com

--- src/components/navigation.ts
+++ src/components/navigation.ts
@@ -14,7 +14,10 @@ export class NavigationManager implements NavigationApi {
     }
 
     public goto(path: string, query: {[name: string]: any} = {}, options?: { event?: React.MouseEvent, replace?: boolean }): void {
-        if (path.startsWith('.')) {
+        if (this.history.location.pathname.endsWith('/') && path.startsWith('./')) {
+            // Prevent ending up with two slashes - e.g. my-argo.test/applications//my-app
+            path = this.history.location.pathname + path.slice(2);
+        } else {
             path = this.history.location.pathname + path.slice(1);
         }
         const noPathChange = path === this.history.location.pathname;
