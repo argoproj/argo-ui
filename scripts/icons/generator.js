72a6f79c776f3d6e1313f2fb0bd6ab63f1ef4973
Omer Azmon <omer_azmon@intuit.com>
2025-05-08 15:59:33 -0700

    upgrade to eslint v9

--- scripts/icons/generator.js
+++ scripts/icons/generator.js
@@ -2,27 +2,42 @@
 
 const webfontsGenerator = require('webfonts-generator');
 const path = require('path');
-const glob = require('glob');
 const fs = require('fs');
-const FONT_TYPES = ['svg', 'ttf', 'woff', 'eot'];
+const execSync = require('child_process').execSync;
+
+const files = fs.readdirSync('src/assets/icons').map(file => path.join('src/assets/icons', file));
+
+const fontPath = path.join('src/assets/fonts', 'argo-icon');
+const fontName = 'argo-icon';
 
 webfontsGenerator({
-    files: glob.sync('src/styles/icons/*.svg'),
-    dest: 'src/assets/fonts',
-    fontName: 'argo-icon',
-    types: FONT_TYPES,
-    cssTemplate: path.resolve(__dirname, './scss.hbs'),
-    templateOptions: {
-        baseTag: 'i',
-        classPrefix: 'argo-icon-',
-        baseSelector: '.argo-icon'
-    }
+    files: files,
+    dest: fontPath,
+    fontName: fontName,
+    cssDest: path.join(fontPath, fontName + '.css'),
+    cssTemplate: 'src/assets/icons-template/template.hbs'
 }, function (error) {
     if (error) {
         console.log('Fail!', error);
     } else {
-        const scss = fs.readFileSync('src/assets/fonts/argo-icon.css', 'utf-8').replace(/url\(\"argo-icon/g, 'url\($argo-icon-fonts-root + \"argo-icon');
+        // fix issue with generated fonts, ensure that quotes are used for glyps path in SVG font, otherwise it fails to render in Safari/Firefox
+        const svgContent = fs.readFileSync(
+            `${fontPath}/${fontName}.svg`
+        ).toString().replace(/glyph-name="([^"]*)"/g, 'glyph-name="$1" unicode="$1"');
+        fs.writeFileSync(`${fontPath}/${fontName}.svg`, svgContent);
+        const scss = fs.readFileSync('src/assets/fonts/argo-icon.scss').toString().replace(new RegExp("url\\(\"./argo-icon"), "url(\"../fonts/argo-icon");
         fs.writeFileSync('src/styles/argo-icon.scss', scss);
         fs.unlinkSync('src/assets/fonts/argo-icon.css');
+        // set generated files permissions to 644 as git treats 755 as changes
+        execSync(
+            'chmod 644 ' + [
+            `${fontName}.svg`,
+            `${fontName}.eot`,
+            `${fontName}.woff2`,
+            `${fontName}.woff`,
+            `${fontName}.ttf`
+        ].map(name => path.join(fontPath, name)).join(' ') + ' && yarn lint' // lint after generation
+        );
+        console.log('Done!');
     }
 });
