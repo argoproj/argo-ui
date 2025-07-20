86bf1fd511f2fa8c4940e52d48f07bae583cf6d5
jiwlee <ddazi9576@gmail.com>
2025-07-20 17:09:36 +0900

    feat: add wildcard filter option to AutoComplete in Storybook
    Signed-off-by: jiwlee <ddazi9576@gmail.com>

--- v2/components/autocomplete/autocomplete.stories.tsx
+++ v2/components/autocomplete/autocomplete.stories.tsx
@@ -9,14 +9,19 @@ export default {
         items: {control: {type: 'object'}},
         inputStyle: {control: {disable: true}},
         placeholder: {control: {type: 'text'}},
+        wildcard: {control: {type: 'boolean'}},
     },
 };
 
-export const Primary = (args: any) => (
-    <div style={{width: '50%', paddingBottom: '6em'}}>
-        <Autocomplete {...args} items={['hello', 'world']} />
-    </div>
-);
+export const Primary = (args: any) => {
+    const [input, setInput] = React.useState('');
+
+    return (
+        <div style={{width: '50%', paddingBottom: '6em'}}>
+            <Autocomplete {...args} items={['hello', 'world', 'hello world', 'hello world2']} value={input} onChange={(e) => setInput(e.target.value)} />
+        </div>
+    );
+};
 
 Primary.args = {
     placeholder: 'Type something here',
