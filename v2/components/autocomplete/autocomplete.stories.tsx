69ffdf668767b2d1d93954558fdb9551c32c4fbb
Jiwon Lee <53519832+jiwlee97@users.noreply.github.com>
2025-10-15 22:35:24 +0900

    feat: Support wildcard filtering in AutoComplete component (#594)
    * feat: add wildcard search support to autocomplete component
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * feat: add wildcard filter option to AutoComplete in Storybook
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * chore: add minimatch@5.1.6
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * refactor: replace manual regex with minimatch for pattern matching
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * feat: apply minimatch when props.wildcard is true
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * feat: update autocomplete to use glob options instead of wildcard
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    * chore: update autocomplete story to use a predefined list of items
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>
    
    ---------
    
    Signed-off-by: jiwlee <ddazi9576@gmail.com>

--- v2/components/autocomplete/autocomplete.stories.tsx
+++ v2/components/autocomplete/autocomplete.stories.tsx
@@ -9,14 +9,41 @@ export default {
         items: {control: {type: 'object'}},
         inputStyle: {control: {disable: true}},
         placeholder: {control: {type: 'text'}},
+        glob: {control: {type: 'boolean'}},
     },
 };
 
-export const Primary = (args: any) => (
-    <div style={{width: '50%', paddingBottom: '6em'}}>
-        <Autocomplete {...args} items={['hello', 'world']} />
-    </div>
-);
+export const Primary = (args: any) => {
+    const [input, setInput] = React.useState('');
+
+    const items = [
+        'User',
+        'AppServer',
+        'AuthService',
+        'Backend',
+        'Cache',
+        'Config',
+        'Database',
+        'Frontend',
+        'Logger',
+        'utils.js',
+        'data-01.csv',
+        'data-02.csv',
+        'data-final.csv',
+        'src/index.js',
+        'src/components/Button.js',
+        'src/components/Input.ts',
+        'test/app.test.js',
+        '.env',
+        'README.md',
+    ];
+
+    return (
+        <div style={{width: '50%', paddingBottom: '6em'}}>
+            <Autocomplete {...args} items={items} value={input} onChange={(e) => setInput(e.target.value)} />
+        </div>
+    );
+};
 
 Primary.args = {
     placeholder: 'Type something here',
