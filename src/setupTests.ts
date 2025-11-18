2885b72521527dd4b9d1c491a08e62723846546a
Jonathan Winters <wintersjonathan0@gmail.com>
2025-11-18 08:22:52 -0800

    fix enzyme tests
    Signed-off-by: Jonathan Winters <wintersjonathan0@gmail.com>

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,7 @@
+import Enzyme from 'enzyme'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
+
+Enzyme.configure({ adapter: new Adapter() });
+
 // Configure testing environment for React 18
 (globalThis as any).IS_REACT_ACT_ENVIRONMENT = true;
