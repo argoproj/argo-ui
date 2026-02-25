3d091a3c07c5732a312db6e7a7ec13a702bdc9c0
Jonathan Winters <wintersjonathan0@gmail.com>
2026-02-25 14:28:38 -0800

    initial react 19 upgrade changes
    Signed-off-by: Jonathan Winters <wintersjonathan0@gmail.com>

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,1 @@
-import Enzyme from 'enzyme'
-import Adapter from '@cfaester/enzyme-adapter-react-18'
-
-Enzyme.configure({ adapter: new Adapter() });
-
-// Configure testing environment for React 18
-(globalThis as any).IS_REACT_ACT_ENVIRONMENT = true;
+import '@testing-library/jest-dom';
